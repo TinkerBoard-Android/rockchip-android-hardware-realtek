9d8e22c5ad9e737b6a6a35bee2af37b148a1e5e9
Rock Shen <rock_shen@asus.com>
2022-05-30 15:40:32 +0800

    Bluetooth: update rtkbt to 20220111_BT_ANDROID_11.0_RTL8822C_COEX_v2020
    Signed-off-by: Rock Shen <rock_shen@asus.com>
    Change-Id: Ibb2ad724e8e825fe8262d31451cd6a06ff675bd3

--- rtkbt/rtkbt.mk
+++ rtkbt/rtkbt.mk
@@ -1,55 +1,71 @@
-# RELEASE NAME: 20180702_BT_ANDROID_9.0
+# RELEASE NAME: 20220111_BT_ANDROID_11.0
 # RTKBT_API_VERSION=2.1.1.0
 
 CUR_PATH := hardware/realtek/rtkbt
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_RTK := true
-BOARD_HAVE_BLUETOOTH_RTK_COEX := true
+BOARD_HAVE_BLUETOOTH_RTK_TV := false
 
-ifneq ($(filter atv box, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(CUR_PATH)/bluetooth
-endif
-ifneq ($(filter rk3328, $(strip $(TARGET_BOARD_PLATFORM))), )
+#ifeq ($(BOARD_HAVE_BLUETOOTH_RTK_TV), true)
+#Firmware For Tv
+#include $(LOCAL_PATH)/Firmware/TV/TV_Firmware.mk
+#else
+#Firmware For Tablet
+#include $(LOCAL_PATH)/Firmware/BT/BT_Firmware.mk
+#endif
+
+#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(CUR_PATH)/bluetooth
+
 PRODUCT_COPY_FILES += \
-        $(CUR_PATH)/vendor/etc/bluetooth/rtkbt_S0.conf:vendor/etc/bluetooth/rtkbt.conf
+       $(CUR_PATH)/vendor/etc/bluetooth/rtkbt.conf:vendor/etc/bluetooth/rtkbt.conf \
+       $(CUR_PATH)/system/etc/permissions/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+       $(CUR_PATH)/system/etc/permissions/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
 
-else
+ifeq ($(BOARD_HAVE_BLUETOOTH_RTK_TV), true)
 PRODUCT_COPY_FILES += \
-	$(CUR_PATH)/vendor/etc/bluetooth/rtkbt.conf:vendor/etc/bluetooth/rtkbt.conf
-
+        $(CUR_PATH)/vendor/usr/keylayout/Vendor_005d_Product_0001.kl:vendor/usr/keylayout/Vendor_005d_Product_0001.kl \
+        $(CUR_PATH)/vendor/usr/keylayout/Vendor_005d_Product_0002.kl:vendor/usr/keylayout/Vendor_005d_Product_0002.kl
 endif
 
-ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), tablet)
-BT_FIRMWARE_FILES := $(shell ls $(CUR_PATH)/vendor/firmware)
 ifeq ($(TARGET_BOARD_PLATFORM),rk3288)
 PRODUCT_COPY_FILES += \
-    $(CUR_PATH)/vendor/firmware/rtl8723bs_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8723bs_config \
-    $(CUR_PATH)/vendor/firmware/rtl8723bs_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8723bs_fw \
-    $(CUR_PATH)/vendor/firmware/rtl8723ds_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8723ds_config \
-    $(CUR_PATH)/vendor/firmware/rtl8723ds_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8723ds_fw
+    $(CUR_PATH)/Firmware/BT/rtl8723bs_config:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8723bs_config \
+    $(CUR_PATH)/Firmware/BT/rtl8723bs_fw:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8723bs_fw \
+    $(CUR_PATH)/Firmware/BT/rtl8723ds_config:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8723ds_config \
+    $(CUR_PATH)/Firmware/BT/rtl8723ds_fw:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8723ds_fw
 else
 PRODUCT_COPY_FILES += \
-    $(CUR_PATH)/vendor/firmware/rtl8822bu_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8822bu_config \
-    $(CUR_PATH)/vendor/firmware/rtl8822bu_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8822bu_fw \
-    $(CUR_PATH)/vendor/firmware/rtl8822cu_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8822cu_config \
-    $(CUR_PATH)/vendor/firmware/rtl8822cu_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8822cu_fw \
-    $(CUR_PATH)/vendor/firmware/rtl8852bu_config:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8852bu_config \
-    $(CUR_PATH)/vendor/firmware/rtl8852bu_fw:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/rtl8852bu_fw
-endif
-else
-BT_FIRMWARE_FILES := $(shell ls $(CUR_PATH)/vendor/firmware_box)
-PRODUCT_COPY_FILES += \
-	$(foreach file, $(BT_FIRMWARE_FILES), $(CUR_PATH)/vendor/firmware_box/$(file):$(TARGET_COPY_OUT_VENDOR)/etc/firmware/$(file))
+    $(CUR_PATH)/Firmware/BT/rtl8822bu_config:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8822bu_config \
+    $(CUR_PATH)/Firmware/BT/rtl8822bu_fw:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8822bu_fw \
+    $(CUR_PATH)/Firmware/BT/rtl8822c_config:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8822c_config \
+    $(CUR_PATH)/Firmware/BT/rtl8822c_fw:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8822c_fw \
+    $(CUR_PATH)/Firmware/BT/rtl8852bu_config:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8852bu_config \
+    $(CUR_PATH)/Firmware/BT/rtl8852bu_fw:$(TARGET_COPY_OUT_VENDOR)/firmware/rtl8852bu_fw
 endif
 
+
+# base bluetooth
 PRODUCT_PACKAGES += \
-	libbt-vendor-realtek
+    Bluetooth \
+    libbt-vendor-realtek \
+    audio.a2dp.default \
+    bluetooth.default \
+    android.hardware.bluetooth@1.0-impl \
+    android.hidl.memory@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth@1.0-service.rc \
+
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.bluetooth.btsnoopenable=false \
-	persist.bluetooth.btsnoopsize=0xffff \
-	persist.bluetooth.rtkcoex=true \
-	bluetooth.enable_timeout_ms=11000
+                    persist.vendor.bluetooth.rtkcoex=true \
+                    persist.vendor.rtkbt.bdaddr_path=none \
+                    persist.vendor.bluetooth.prefferedrole=master \
+                    persist.vendor.rtkbtadvdisable=false
 
-
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.bluetooth.btsnooplogmode=disable \
+                    persist.bluetooth.btsnooppath=/data/misc/bluedroid/btsnoop_hci.cfa \
+                    persist.bluetooth.btsnoopsize=0xffff \
+                    persist.bluetooth.showdeviceswithoutnames=false \
+                    vendor.bluetooth.enable_timeout_ms=11000 \
+                    vendor.realtek.bluetooth.en=false
